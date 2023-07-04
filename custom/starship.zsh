c0c99f8e8fa4a480a4aa592572842d649904f654
Jan Schmidt <hi@jnsmdt.dev>
2023-07-04 22:45:51 +0200

    i'm trying things

--- custom/starship.zsh
+++ custom/starship.zsh
@@ -2,9 +2,8 @@
 function set_win_title() {
 
 	title="${PWD:t2}"
-
-	if [ -z ${JNSMDT_SERVERNAME+x} ]; then
-		title="$JNSMDT_SERVERNAME - ${PWD:t2}"
+	if [[ -$parameters[JSERVERNAME]- = *-export-* ]]; then
+		title="${JSERVERNAME} - ${PWD:t2}"
 	fi
 
 	echo -ne "\033]0; "$title" \007"
