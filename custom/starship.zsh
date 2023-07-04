1b8e14d383b60a8d9cd5c46d4ccaa7098c4092bc
Jan Schmidt <hi@jnsmdt.dev>
2023-07-04 04:54:40 +0200

    modified win_title_function

--- custom/starship.zsh
+++ custom/starship.zsh
@@ -1,13 +1,13 @@
 ## Set Tabname to Directory in Starship
 function set_win_title() {
 
-	title="$PWD"
+	title="${PWD:t2}"
 
 	if [ -z ${JNSMDT_SERVERNAME+x} ]; then
-		title="$JNSMDT_SERVERNAME - $PWD"
+		title="$JNSMDT_SERVERNAME - ${PWD:t2}"
 	fi
 
-	echo -ne "\033]0; $(expr "$title" : '.*/\([^/]*/[^/]*\)$') \007"
+	echo -ne "\033]0; "$title" \007"
 }
 
 precmd_functions+=(set_win_title)
