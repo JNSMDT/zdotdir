eb9584b76e588f3a5463afcfb9d24181ab528dee
Jan Schmidt <hi@jnsmdt.dev>
2023-07-04 03:53:25 +0200

    added new servername to tabtitle

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ My small zsh config dir. To use create a `.zshenv` file inside your home directo
 After that add following snippit into it, so it uses your your own `.zshenv`.
 
 ```BASH
+export JNSDMT_SERVERNAME=""
+
 export ZDOTDIR=~/.config/zsh
 [[ -f $ZDOTDIR/.zshenv ]] && . $ZDOTDIR/.zshenv
 ```
--- custom/starship.zsh
+++ custom/starship.zsh
@@ -1,7 +1,13 @@
-
 ## Set Tabname to Directory in Starship
 function set_win_title() {
-	echo -ne "\033]0; $(expr "$PWD" : '.*/\([^/]*/[^/]*\)$') \007"
+
+	title="$PWD"
+
+	if [ -z ${JNSMDT_SERVERNAME+x} ]; then
+		title="$JNSMDT_SERVERNAME - $PWD"
+	fi
+
+	echo -ne "\033]0; $(expr "$title" : '.*/\([^/]*/[^/]*\)$') \007"
 }
 
 precmd_functions+=(set_win_title)
