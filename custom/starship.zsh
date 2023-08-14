ea9cd7f726c217540519474bb3991f6214b9161d
Jan Schmidt <hi@jnsmdt.dev>
2023-08-14 13:22:55 +0200

    updated win title function

--- custom/starship.zsh
+++ custom/starship.zsh
@@ -1,7 +1,16 @@
 ## Set Tabname to Directory in Starship
 function set_win_title() {
-	title="$HOST - ${PWD:t2}"
-	echo -ne "\033]0; "$title" \007"
+	title="${PWD:t2}"
+
+	# if title is too long, show only current folder
+	if [ ${#title} -gt 20 ]; then
+		title="${PWD:t}"
+	fi
+
+	# get only upperase letter from $HOST variable
+	HOST=$(echo $HOST | tr -dc '[:upper:]')
+
+	echo -ne "\033]0; "$HOST \| $title" \007"
 }
 
 precmd_functions+=(set_win_title)
