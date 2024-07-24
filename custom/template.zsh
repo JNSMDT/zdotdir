57b42f949bff27aa6ee0bc6eeddea68058939cd4
Jan Schmidt <hi@jnsmdt.dev>
2024-07-24 20:04:39 +0200

    add dir check and option to select template without template prefix

--- custom/template.zsh
+++ custom/template.zsh
@@ -2,7 +2,18 @@ function template {
 	template_name=$1
 	destination=$2
 
-	template_path="$HOME/dev/templates/$1/"
+	# check if tempalte_name comes with template prefix
+	if [[ ! $template_name =~ "template" ]]; then
+		template_name="template-$template_name"
+	fi
+
+	template_path="$HOME/dev/templates/$1"
+
+	# check if template_path exists
+	if [ ! -d "$template_path" ]; then
+		echo "Template $template_name not found"
+		return 1
+	fi
 
 	rsync -av $template_path $2 \
 		--exclude .git \
